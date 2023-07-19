96493cd14302189874212a6ab260b62fd3e781d3
Noelle <noelle.maingi@student.moringaschool.com>
2023-07-19 19:30:14 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {v4 as uuid} from "uuid"
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map((link)=> <a href={`#${link}`} key={uuid()}>{link}</a> )}</nav>;
 }
 
 export default NavBar;
